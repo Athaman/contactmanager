281c026dc51168ecb0440018cdd2fd1b70439009
Keone Martin <keonemartin@gmail.com>
2018-02-12 22:17:45 +1100

    finished setting up the displays for the contacts brought in from the api call

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
+import {ListGroup} from 'react-bootstrap'
+import ContactListItem from './ContactListItem';
 import AppActions from '../actions/AppActions';
 import AppStore from '../stores/AppStore';
+
+function getContactListItem(contact) {
+  return (
+    <ContactListItem key={contact.id} contact={contact} />
+  );
+}
 
 class Contacts extends Component {
   constructor(props) {
@@ -33,9 +41,15 @@ class Contacts extends Component {
   }
 
   render() {
+    let contactListItems;
+    if(this.state.contacts){
+      contactListItems = this.state.contacts.map(contact => getContactListItem(contact));
+    }
     return (
       <div>
-        CONTACTS
+        <ListGroup>
+          {contactListItems}
+        </ListGroup>
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {Navbar} from 'react-bootstrap';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 class Header extends Component {
   render() {
     return (
